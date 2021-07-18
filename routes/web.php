eef8a2a9bf817147c80bf1d02e36de276ef6da2d
Nur Wachid <wachid@outlook.com>
2021-07-18 15:37:30 +0700

    add funding url

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('send_test_email', function(){
+    Mail::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
+    {
+        $message->to('johndoe@gmail.com');
+    });
+});
